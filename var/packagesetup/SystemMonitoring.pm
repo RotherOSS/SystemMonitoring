d6fc53e6eb22f18a07c6941b1313ca2b4a1892ff
Stefan Härter <sha@rother-oss.com>
2024-03-27 16:00:50 +0100

    Updated package via CodePolicy.

--- var/packagesetup/SystemMonitoring.pm
+++ var/packagesetup/SystemMonitoring.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2024 Rother OSS GmbH, https://otobo.de/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -361,7 +361,7 @@ sub _GetDynamicFieldsDefinition {
                 if ( !$Run ) {
                     $Kernel::OM->Get('Kernel::System::Log')->Log(
                         Priority => 'error',
-                        Message =>
+                        Message  =>
                             "Execute GetDynamicFieldsDefinition() of $Key $Jobs{$Job}->{Module} not successful!",
                     );
                 }
